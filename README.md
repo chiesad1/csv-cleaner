5ec5748c2c3fbe275f2c08b2e3e7a9f5bc4709c6
Daniel Chiesa <chiesad1@gmail.com>
2026-03-23 21:02:30 -0400

    initial structure with raw data

---
+++ README.md
@@ -0,0 +1,2 @@
+# csv-cleaener
+# csv-cleaener
---
+++ csv_cleaner.py
@@ -0,0 +1,19 @@
+#Should be able to read CSV files and removes empty rows 
+#and spits output
+
+import csv
+
+#open the csv file
+with open ('example.csv', mode = 'r') as file:
+
+    #Create a CSV reader object
+    csv_reader = csv.reader(file)
+    
+    #read the header    
+    header = next(csv_reader)
+    print(f"Header: {header}")
+
+    #Read each row of the CSV file
+    for row in csv_reader:
+        print(f"Row: {row}")
+
