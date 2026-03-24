b8cc486853eec4e6fb3a01192abfa49d42cc74b7
Daniel Chiesa <danielchiesa@Daniels-MacBook-Air-2.local>
2026-03-24 12:03:37 -0400

    Reintroduced regex and used it to get rid of $ and added float conversion

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -5,8 +5,23 @@ import csv
 
 #path name so you can output cleaned data 
 
-import os 
+import os
+#imports Regex
+import re
 
+
+def clean_row(row):
+    price = row['Price']
+    if price =='':
+    #if there is no price put none 
+        row['Price'] = None
+    else:
+   
+      price = re.sub(r'[^\d.]','', price)
+      #price = price.replace('$','')
+      row['Price'] = float(price)
+    return row
+    
 
 #open the csv file
 with open ('example.csv', mode = 'r') as file:
@@ -15,20 +30,17 @@ with open ('example.csv', mode = 'r') as file:
     csv_reader = csv.DictReader(file)
     
     #read the header    
-    header = next(csv_reader)
-    print(f"Header: {header}")
+    #header = next(csv_reader)
+    #print(f"Header: {header}")
 
     #Read each row of the CSV file
     for row in csv_reader:
-        print(f"Row: {row}")
+    #old function    
+    #print(f"Row: {row}")
+        #new function
+        cleaned = clean_row(row)
+        print(cleaned)
 
-price = row['Price']
-if row['Price'] =='':
-    #if there is no price put none 
-        print(f"none")
-else:
-   # converts to float
-       price.replace('$','')
 
 f = open("output.txt", "w")
 
